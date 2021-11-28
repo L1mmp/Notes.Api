2f0147dd2d9b721be4421789f0f534496d0c9d55
L1mmp <dima.wwe2@yandex.ru>
2021-11-28 18:39:29 +0300

    Changed string[] to hashset for unique values

--- Notes.DataAccess/Entites/Note.cs
+++ Notes.DataAccess/Entites/Note.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace Notes.DataAccess.Entites
 {
     public class Note
     {
         [Key]
-        public Guid Id { get; set; }
-        public string[]? Hashtags { get; set; }
+        public int Id { get; set; }
+        public HashSet<string> Hashtags { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
         public DateTime CreationDate { get; set; }
--- Notes.Domian/Models/Note.cs
+++ Notes.Domian/Models/Note.cs
@@ -6,11 +6,8 @@ namespace Notes.Domian.Models
 {
     public class Note
     {
-        public string[]? Hashtags { get; set; }
+        public HashSet<string> Hashtags { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
-        public DateTime CreationDate { get; set; }
-        public DateTime? LastEditionDate { get; set; }
-
     }
 }
