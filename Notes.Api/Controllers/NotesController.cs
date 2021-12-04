b83ba43d9d90aa4453192899c364138ee55f4b78
L1mmp <dima.wwe2@yandex.ru>
2021-12-04 09:03:36 +0300

    Added routings for controller methods

--- Notes.Api/Controllers/NotesController.cs
+++ Notes.Api/Controllers/NotesController.cs
@@ -17,37 +17,71 @@ namespace Notes.Api.Controllers
             _noteService = noteService;
         }
 
+        /// <summary>
+        /// Create note
+        /// </summary>
+        /// <param name="note">Note object</param>
+        /// <returns></returns>
         [HttpPost]
+        [Route("Create")]
         public ActionResult<bool> Create(Note note)
         {
             return Ok(_noteService.Create(note));
         }
 
-        [HttpGet("{title}")]
+        /// <summary>
+        /// Get notes with title
+        /// </summary>
+        /// <param name="title"> Note title</param>
+        /// <returns>Notes</returns>
+        [HttpGet]
+        [Route("Get/{title}")]
         public ActionResult<Note[]> GetByTitle(string title)
         {
             return Ok(_noteService.GetByTitle(title));
         }
 
-        [HttpGet("{id}")]
+        /// <summary>
+        /// Get notes with id
+        /// </summary>
+        /// <param name="id">Note id</param>
+        /// <returns>Note</returns>
+        [HttpGet]
+        [Route("Get/{id:int}")]
         public ActionResult<Note> GetById(int id)
         {
             return Ok(_noteService.GetById(id));
         }
 
+        /// <summary>
+        /// Update note with id
+        /// </summary>
+        /// <param name="id"> Note id</param>
+        /// <param name="note">Note object</param>
         [HttpPut]
+        [Route("Update")]
         public void Update(int id, Note note)
         {
             _noteService.Update(id, note);
         }
 
+        /// <summary>
+        /// Get all notes
+        /// </summary>
+        /// <returns>All notes</returns>
         [HttpGet]
+        [Route("GetAll")]
         public ActionResult<Note[]> GetAllNotes()
         {
             return Ok(_noteService.GetAllNotes());
         }
 
+        /// <summary>
+        /// Delete note with id 
+        /// </summary>
+        /// <param name="id">Note id</param>
         [HttpDelete]
+        [Route("Delete")]
         public void Delete(int id)
         {
             _noteService.Delete(id);
